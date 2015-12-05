7e747aed8c5914bd171e3ef81a2034aab5eb3a70
jcampolineu <jcampoli@ccs.neu.edu>
2015-12-05 15:59:03 -0500

    Some more minor files/packages cleanup

--- clean.bat
+++ clean.bat
@@ -1,6 +1,6 @@
 @echo on
-Remove-Item -Recurse -Force .\packages
-Remove-Item -Recurse -Force .\NextMile02\obj
-Remove-Item -Recurse -Force .\NextMile02\bin
-Remove-Item -Recurse -Force .\NextMile02.Tests\obj
-Remove-Item -Recurse -Force .\NextMile02.Tests\bin
+del -Recurse -Force .\packages\
+#Remove-Item -Recurse -Force .\NextMile02\obj
+#Remove-Item -Recurse -Force .\NextMile02\bin
+#Remove-Item -Recurse -Force .\NextMile02.Tests\obj
+#Remove-Item -Recurse -Force .\NextMile02.Tests\bin
