20c3a8bf3d2c8f9a96cb481e32ae14a7e21970cc
shivastuti koul <koulsh16@ccs.neu.edu>
2015-12-07 14:48:11 -0500

    setting image url - checking issue

--- NextMile02/Scripts/javascript/Facebook.js
+++ NextMile02/Scripts/javascript/Facebook.js
@@ -102,6 +102,12 @@ function InitialiseFacebook(appId)
         }, 600);
 
     });
+    if (($(window).height() + 100) < $(document).height()) {
+        $('#top-link-block').removeClass('hidden').affix({
+            // how far to scroll down before link "slides" into view
+            offset: { top: 100 }
+        });
+    }
     (function (d)
     {
         var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
